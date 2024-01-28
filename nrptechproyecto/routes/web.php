322666f51140bc96fdfc9296138abd8dfeae8ddb
Nicolás Gómez García <nicogomezgarcia1@gmail.com>
2024-01-28 18:18:21 +0100

    rama temporal

--- nrptechproyecto/routes/web.php
+++ nrptechproyecto/routes/web.php
@@ -26,3 +26,7 @@ Route::get('/home', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
--- nrptechproyecto/vite.config.js
+++ nrptechproyecto/vite.config.js
@@ -1,19 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from "path"
+import path from 'path';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: [
+        laravel([
                 'resources/js/app.js',
             ],
-            refresh: true,
-        }),
+        ),
     ],
-    resolve: {
-        alias: {
+    resolve:{
+        alias:{
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
         }
-    },
-});
+    }
+}); 
