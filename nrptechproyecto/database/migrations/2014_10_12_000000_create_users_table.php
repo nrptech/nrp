f1c4d53f658171fd4af555c351218aec23836ca5
Rafael Pedrosa <blas_tois@alumnos.ilernasevilla.com>
2024-01-31 11:01:25 +0100

    ffd

--- nrptechproyecto/database/migrations/2014_10_12_000000_create_users_table.php
+++ nrptechproyecto/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->unsignedBigInteger("role_id")->default(2);
             $table->rememberToken();
             $table->timestamps();
+
             
         });
     }
