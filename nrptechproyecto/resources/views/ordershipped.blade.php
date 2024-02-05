4984f1a83e2f540228fd33dfa9e4155b3ff4c1c4
fcorguez92 <fcorguezsevilla@gmail.com>
2024-02-05 23:09:22 +0100

    prettier mail

--- nrptechproyecto/resources/views/ordershipped.blade.php
+++ nrptechproyecto/resources/views/ordershipped.blade.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="utf-8">
-    <title>@yield('title', 'Seguimiento de compra')</title>
-</head>
-<body>
-   HOLA HOLA ESTE MENSAJE SE MANDA HOLAAAAAAA
-</body>
-</html>
+@extends('layouts.email')
+
+@section('content')
+<h1>Seguimiento de compra</h1>
+
+<p>Gracias por tu compra. Aquí está la información de tu pedido:</p>
+
+<p>Su pedido se encuentra de camino!</p>
+<ul>
+    <li><strong>Número de pedido:</strong> ID DE LA ORDEN</li>
+    <li><strong>Total:</strong> TOTAL DE LA ORDEN</li>
+</ul>
+
+<p>¡Gracias por elegirnos!</p>
+@endsection
