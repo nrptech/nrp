887fd1a7c11ac4ce4161893fd292a32dd2cbca58
NRP tech <nrpcontacto@gmail.com>
2024-02-12 14:17:52 +0100

    me desespera esto no va el boton

---
+++ nrptechproyecto/app/Http/Controllers/LanguageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+
+class LanguageController extends Controller
+{
+    public function switchLanguage($language)
+    {
+        // Validate that the requested language is supported
+        $supportedLanguages = ['es', 'en'];
+        if (!in_array($language, $supportedLanguages)) {
+            abort(400, 'Invalid language');
+        }
+
+        // Set the application locale to the requested language
+        App::setLocale($language);
+
+        // Store the language preference in the session
+        Session::put('locale', $language);
+
+        // Redirect back to the previous page or any specific page
+        return redirect()->back();
+    }
+}
--- nrptechproyecto/routes/web.php
+++ nrptechproyecto/routes/web.php
@@ -1,7 +1,5 @@
 <?php
-
 use App\Http\Controllers\CartController;
-use App\Models\Cart;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
@@ -10,58 +8,60 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\LanguageController;
 
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Home route
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Authentication routes
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Middleware for setting the locale
 
-Route::middleware(['auth'])->get('/products', [Product::class, 'showProducts'])->name("products");
+    // Authenticated routes
+    Route::middleware(['auth'])->group(function () {
+        // Home
+        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+        // Products
+        Route::get('/products', [Product::class, 'showProducts'])->name("products");
+        // Cart routes
+        Route::get('/cart', [CartController::class, 'showCart'])->name('cart');
+        Route::post('/add-to-cart/{product}', [CartController::class, 'addToCart'])->name('cart.add');
+        Route::post('/cart', [CartController::class, 'updateCart'])->name('cart.update');
+        Route::post('/substrac-amount/{product}', [CartController::class, 'substracAmount'])->name('cart.substracAmount');
+        // Checkout
+        Route::get('/checkout', 'CheckoutController@index')->name('checkout');
+        // Messages
+        Route::post('/messages/store', [MessagesController::class, 'store'])->name('messages.store');
+        // Order
+        Route::get('/order', [CartController::class, 'showOrder'])->name('order.show');
+        // Invoice
+        Route::get('/invoice', [OrderController::class, 'showInvoice'])->name('invoice.show');
+        Route::get('/invoice/create', [InvoiceController::class, 'create'])->name('invoice.create');
+        Route::get('/invoice/show', [InvoiceController::class, 'show'])->name('invoice.show');
+        // Gracias por comprar
+        Route::get('/gracias-por-comprar', [CartController::class, 'mostrarAgradecimiento'])->name('agradecimiento');
+        // Product update and destroy
+        Route::put('/productos/{product}', [ProductController::class, 'update'])->name('productos.update');
+        Route::delete('/productos/{product}', [ProductController::class, 'destroy'])->name('productos.destroy');
 
-Route::middleware(["auth"])->get('/cart', [CartController::class, 'showCart'])->name('cart');
+        // Admin routes
+        Route::middleware(['role:admin'])->group(function () {
+            // Roles, Users, and Category
+            Route::resource('roles', RoleController::class);
+            Route::resource('productos', ProductController::class);
+            Route::resource('users', UserController::class);
+            Route::resource('category', ProductController::class);
+        });
+    });
+Route::middleware(['checkLocale'])->group(function () {
+    // This is the correct place for the language switch route
+    Route::get('/switch-language/{language}', [LanguageController::class, 'switchLanguage'])
+        ->name('switch.language');
 
-Route::post('/add-to-cart/{product}', [CartController::class, 'addToCart'])->name('cart.add');
-
-Route::post('/cart', [CartController::class, 'updateCart'])->name('cart.update');
-
-Route::post('/substrac-amount/{product}', [CartController::class, 'substracAmount'])->name('cart.substracAmount');
-
-// Route::post('/cart/remove/{product}', 'CartController@removeFromCart')->name('cart.remove');
-
-Route::get('/checkout', 'CheckoutController@index')->name('checkout');
-Route::post('/messages/store', [MessagesController::class, 'store'])->name('messages.store');
-Route::get('/order', [CartController::class, 'showOrder'])->name('order.show');
-Route::post('/order/confirm', [CartController::class, 'confirmOrder'])->name('confirmOrder');
-Route::post('/order/reject', [CartController::class, 'rejectOrder'])->name('rejectOrder');
-Route::get('/cart', [CartController::class, 'showCart'])->name('cart.show');
-Route::get('/invoice', [OrderController::class, 'showInvoice'])->name('invoice.show');
-Route::get('/invoice/create', [InvoiceController::class, 'create'])->name('invoice.create');
-Route::get('/invoice/show', [InvoiceController::class, 'show'])->name('invoice.show');
-Route::get('/gracias-por-comprar', [CartController::class, 'mostrarAgradecimiento'])->name('agradecimiento');
-
-Route::put('/productos/{product}', [ProductController::class, 'update'])->name('productos.update');
-
-Route::delete('/productos/{product}', [ProductController::class, 'destroy'])->name('productos.destroy');
-
-Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::resource('roles', RoleController::class);
-    Route::resource('productos', ProductController::class);
-    Route::resource('users', UserController::class);
-    Route::resource('category', ProductController::class);
+         // Language switch route
+    Route::get('/switch-language/{language}', [LanguageController::class, 'switchLanguage'])
+    ->name('switch.language');
 });
