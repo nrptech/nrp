093b90f9b252faa084c581d7eccae3899dd94e16
NRP tech <nrpcontacto@gmail.com>
2024-02-13 14:00:09 +0100

    foorms empezado la verificacion

--- nrptechproyecto/resources/views/auth/login.blade.php
+++ nrptechproyecto/resources/views/auth/login.blade.php
@@ -30,34 +30,29 @@
                                         
                                     </div>
 
-                                    <form action="{{route("login")}}" method="post">
-                                        @csrf
-                                        <p>Iniciar sesión:</p>
+                                    <form action="{{ route('login') }}" method="post">
+        @csrf
+        <p>Iniciar sesión:</p>
 
-                                        <div class="form-outline mb-4">
-                                            <label class="form-label" for="form2Example11">Email</label>
-                                            <input type="email" name="email" id="form2Example11" class="form-control"
-                                                placeholder="Correito@gmail.com" />
+        <div class="form-outline mb-4">
+            <label class="form-label" for="form2Example11">Email</label>
+            <input type="email" name="email" id="form2Example11" class="form-control" placeholder="Correito@gmail.com" required />
+        </div>
 
-                                        </div>
+        <div class="form-outline mb-4">
+            <label class="form-label" for="form2Example22">Password</label>
+            <input type="password" name="password" id="form2Example22" class="form-control" required />
+        </div>
 
-                                        <div class="form-outline mb-4">
-                                            <label class="form-label" for="form2Example22">Password</label>
-                                            <input type="password" name="password" id="form2Example22" class="form-control" />
+        <div class="text-center pt-1 mb-5 pb-1">
+            <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">Log in</button>
+        </div>
 
-                                        </div>
-
-                                        <div class="text-center pt-1 mb-5 pb-1">
-                                            <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">Log
-                                              in</button>
-                                          </div>
-
-                                        <div class="d-flex align-items-center justify-content-center pb-4">
-                                            <p class="mb-0 me-2">¿No tienes cuenta?</p>
-                                            <a href="{{route("register")}}"><button type="button" class="btn btn-outline-danger">Registrate</button></a>
-                                        </div>
-
-                                    </form>
+        <div class="d-flex align-items-center justify-content-center pb-4">
+            <p class="mb-0 me-2">¿No tienes cuenta?</p>
+            <a href="{{ route('register') }}"><button type="button" class="btn btn-outline-danger">Registrate</button></a>
+        </div>
+    </form>
 
                                 </div>
                             </div>
