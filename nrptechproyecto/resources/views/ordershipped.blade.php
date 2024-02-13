846f89149db7a197ece6692c6daaf0571fbcf181
fcorguez92 <fcorguezsevilla@gmail.com>
2024-02-13 13:06:59 +0100

    Carrito funcionando a falta de ticket

---
+++ nrptechproyecto/resources/views/ordershipped.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.layout')
+
+@section('title', 'Home')
+
+@section('content')
+
+    <h1 class="mb-4">¡Gracias por su Compra!</h1>
+
+    <p class="alert alert-success">Su compra se ha realizado con éxito. Agradecemos su preferencia.</p>
+
+@endsection
