ce5b7f87dc798fba88a801e0348527ee5bf4186e
fcorguez92 <fcorguezsevilla@gmail.com>
2024-02-15 09:46:48 +0100

    Base style wishlist

--- nrptechproyecto/resources/views/wishlist/index.blade.php
+++ nrptechproyecto/resources/views/wishlist/index.blade.php
@@ -2,31 +2,35 @@
 @section('title', 'Wishlist')
 
 @section('content')
-<h1>Wishlist</h1>
-@if(session('success'))
-    <div style="color: green;">{{ session('success') }}</div>
-@endif
+    <div class="container mt-5">
+        <h1>Wishlist</h1>
 
-@if(session('error'))
-    <div style="color: red;">{{ session('error') }}</div>
-@endif
+        @if(session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
 
-@if($wishlistProducts->isNotEmpty()) 
-    <ul>
-        @foreach($wishlistProducts as $wishlist) 
-          @foreach($wishlist->products as $product) 
-            <li>
-                {{ $product->name }} - ${{ $product->price }} 
-                                <form action="{{ route('wishlist.remove', ['productId' => $product->id]) }}" method="post">
-                    @csrf
-                    <button type="submit">Quitar de Wishlist</button>
-                  </form>
-            </li>
-          @endforeach
-        @endforeach
-    </ul>
-@else
-    <p>No hay productos en la Wishlist.</p>
-@endif
+        @if(session('error'))
+            <div class="alert alert-danger">{{ session('error') }}</div>
+        @endif
 
+        @if($wishlistProducts->isNotEmpty()) 
+            <ul class="list-group mt-3">
+                @foreach($wishlistProducts as $wishlist) 
+                    @foreach($wishlist->products as $product) 
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            <span>
+                                {{ $product->name }} - ${{ $product->price }}
+                            </span>
+                            <form action="{{ route('wishlist.remove', ['productId' => $product->id]) }}" method="post">
+                                @csrf
+                                <button type="submit" class="btn btn-danger">Quitar de Wishlist</button>
+                            </form>
+                        </li>
+                    @endforeach
+                @endforeach
+            </ul>
+        @else
+            <p class="mt-3">No hay productos en la Wishlist.</p>
+        @endif
+    </div>
 @endsection
