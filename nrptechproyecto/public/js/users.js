251baa666f18acd0c41c3b051de0588683cc6787
Rafa <blas_tois@alumnos.ilernasevilla.com>
2024-02-26 14:06:21 +0100

    Mini arreglo en js para poder editar los usuarios de uno en uno

--- nrptechproyecto/public/js/users.js
+++ nrptechproyecto/public/js/users.js
@@ -1,13 +1,28 @@
 "use strict"
 
-function editUser(userId){
-    let toEdit ="edit" + userId
-    let toShow = "view" + userId
-    console.log(toEdit)
+let currentUserEditing = null;
+
+function editUser(userId) {
+    let toEdit = "edit" + userId;
+    let toShow = "view" + userId;
+    console.log(toEdit);
+
+    if (currentUserEditing) {
+        let previousView = "view" + currentUserEditing;
+        let previousEdit = "edit" + currentUserEditing;
+        
+        let previousElementToShow = document.getElementById(previousView);
+        let previousElementToEdit = document.getElementById(previousEdit);
+        
+        previousElementToShow.removeAttribute("hidden");
+        previousElementToEdit.setAttribute("hidden", "hidden");
+    }
 
     let elementToShow = document.getElementById(toShow);
     let elementToEdit = document.getElementById(toEdit);
 
     elementToEdit.removeAttribute("hidden");
-    elementToShow.setAttribute("hidden","hidden");
+    elementToShow.setAttribute("hidden", "hidden");
+
+    currentUserEditing = userId;
 }
