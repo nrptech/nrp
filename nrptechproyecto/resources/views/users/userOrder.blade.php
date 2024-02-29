dfe3b09f6fabd65ee74ec950a487d7b788cad0f0
fcorguez92 <fcorguezsevilla@gmail.com>
2024-02-29 12:36:52 +0100

    Mejora estilo

--- nrptechproyecto/resources/views/users/userOrder.blade.php
+++ nrptechproyecto/resources/views/users/userOrder.blade.php
@@ -11,6 +11,7 @@
             margin-bottom: 20px;
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 20px;
         }
 
         .order-number {
@@ -22,6 +23,7 @@
 
         .product-list {
             width: 100%;
+            margin-top: 20px;
         }
 
         .product-list th,
@@ -35,6 +37,14 @@
             background-color: #f8f9fa;
         }
 
+        .product-list td:nth-child(2) {
+            text-align: right;
+        }
+
+        .product-list th:nth-child(2) {
+            text-align: right;
+        }
+
         .product-name {
             font-weight: bold;
         }
@@ -44,15 +54,20 @@
             margin-top: 10px;
             font-weight: bold;
             color: #333;
+            text-align: right;
         }
 
         .imgMiniature {
             width: 40px;
             margin-right: 10px;
+            border-radius: 20px;
         }
 
-        .imgMiniature img {
-            border-radius: 20px;
+        .order-status {
+            margin-top: 10px;
+            font-size: 1em;
+            color: #555;
+            text-align: right;
         }
     </style>
 @endsection
@@ -76,7 +91,8 @@
                         @foreach ($order->products as $product)
                             <tr>
                                 <td>
-                                    <img src="{{ $product->images->first()->url }}" alt="{{ $product->name }}" class="imgMiniature">
+                                    <img src="{{ $product->images->first()->url }}" alt="{{ $product->name }}"
+                                        class="imgMiniature">
                                     <span class="product-name">{{ $product->name }}</span>
                                 </td>
                                 <td>${{ $product->price }}</td>
@@ -85,11 +101,11 @@
                     </tbody>
                 </table>
 
-                <p class="order-status">Estado del pedido: {{ $order->state }}</p>
-
-                @if ($order->invoice)
-                    <p class="total-price">Precio total del pedido: ${{ $order->invoice->total }}</p>
+                
+                @if ($order->invoice->total)
+                <p class="total-price">Total + IVA: ${{ $order->invoice->total }}</p>
                 @endif
+                <p class="order-status">Estado actual: {{ $order->state }}</p>
             </div>
         @endforeach
     </div>
