367f2f6bb12084884f2e7078eb4237ca4044f190
fcorguez92 <fcorguezsevilla@gmail.com>
2024-03-01 01:10:22 +0100

    Arreglo footer y vista wishlist

--- nrptechproyecto/resources/views/wishlist/index.blade.php
+++ nrptechproyecto/resources/views/wishlist/index.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.layout')
 @section('title', 'Wishlist')
-
+@section('links')
+    <!-- Replace the Bootstrap 4 link with Bootstrap 5 links -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.7.0/dist/css/bootstrap.min.css">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.7.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script defer src="{{ asset('js/home.js') }}"></script>
+    <!-- Include Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('styles/wishlist.css') }}">
+@endsection
 @section('content')
     <div class="container mt-5">
         <h1>Wishlist</h1>
@@ -24,13 +32,24 @@
                 @foreach($wishlistProducts as $wishlist) 
                     @foreach($wishlist->products as $product) 
                         <li class="list-group-item d-flex justify-content-between align-items-center">
-                            <span>
-                                {{ $product->name }} - ${{ $product->price }}
-                            </span>
-                            <form action="{{ route('wishlist.remove', ['productId' => $product->id]) }}" method="post">
-                                @csrf
-                                <button type="submit" class="btn btn-danger">Quitar de Wishlist</button>
-                            </form>
+                            <div class="imgMiniature mx-auto">
+                                @if ($product->images->isNotEmpty())
+                                    <img src="{{ asset($product->images->first()->url) }}" alt="{{ $product->name }}" class="img-fluid" id="img{{ $product->id }}-0">
+                                @endif
+                            </div>
+                            <div class="flex-grow-1 ms-3">
+                                <span class="d-block">{{ $product->name }}</span>
+                                <span class="d-block">$ {{ $product->price }}</span>
+                            </div>
+                            <div class="d-flex flex-column align-items-end">
+                                <a href="{{ route('products.show', $product) }}" class="btn btn-warning mb-2">
+                                    <i class="fas fa-search"></i> Visitar
+                                </a>
+                                <form action="{{ route('wishlist.remove', ['productId' => $product->id]) }}" method="post">
+                                    @csrf
+                                    <button type="submit" class="btn btn-danger">Quitar de Wishlist</button>
+                                </form>
+                            </div>
                         </li>
                     @endforeach
                 @endforeach
